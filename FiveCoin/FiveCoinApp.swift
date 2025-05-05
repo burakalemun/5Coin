f8d19d308db54bf0a6d8731ce2bbf4f581b34ebd
burakalemun <61062491+burakalemun@users.noreply.github.com>
2025-05-05 17:30:37 +0300

    Initial Commit

---
+++ FiveCoin/FiveCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiveCoinApp.swift
+//  FiveCoin
+//
+//  Created by Burak Kaya on 5.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct FiveCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
