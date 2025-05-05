1ea85be3c3907c762ec284b267f8405e76b87e4a
burakalemun <61062491+burakalemun@users.noreply.github.com>
2025-05-05 17:30:37 +0300

    Initial Commit

---
+++ FiveCoinTests/FiveCoinTests.swift
@@ -0,0 +1,17 @@
+//
+//  FiveCoinTests.swift
+//  FiveCoinTests
+//
+//  Created by Burak Kaya on 5.05.2025.
+//
+
+import Testing
+@testable import FiveCoin
+
+struct FiveCoinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
